b7dc05f620138cc068057cb6bea71a1c28821cde
Xiaoze Liu <xiaoze@purdue.edu>
2024-08-05 20:35:51 -0400

    Update agent.py

--- shield/agent.py
+++ shield/agent.py
@@ -1,12 +1,16 @@
 import copy
 import os
 
+import torch.cuda
+# CUDA_VISIBLE_DEVICES=1 python agent.py
+# os.environ['HF_HOME'] = '/local/scratch/f/xiaoze/huggingface_cache'
 
 
 from huggingface_hub import login
 from transformers import AutoTokenizer
 import regex
 
+# login(token=os.environ['HF_TOKEN'])
 
 from typing import List
 from langchain_google_community import GoogleSearchAPIWrapper
@@ -32,6 +36,8 @@ from models import load_or_train_ngram_model, train_ngram_model
 from utils import *
 from public_records import search_book
 from api_chatbots import APIModelsWrapper
+
+from time import time, perf_counter
 
 
 class LicensedMaterial(BaseModel):
@@ -60,10 +66,10 @@ def search_copyright_status_with_pplx_json(title):
     '''
     Search the copyright status of a book with Perplexity API llama-3-sonar-large-32k-online
     '''
-    wrapper = APIModelsWrapper('llama-3-sonar-large-32k-online', sleep_time=4)
-    obj= License()
+    wrapper = APIModelsWrapper('llama-3-sonar-large-32k-online', sleep_time=4, force_use_cache=True)
+    obj = License()
     prompt = (
-            f'You are a helpful assistant. Can you tell me the copyright status of the book {title}? Answer with a JSON String formatted as: \n\n```\n{{{obj.json()}}}\n```'
+        f'You are a helpful assistant. Can you tell me the copyright status of the book {title}? Answer with a JSON String formatted as: \n\n```\n{{{obj.json()}}}\n```'
     )
 
     return wrapper.invoke(prompt)
@@ -132,6 +138,43 @@ class CopyrightAgent:
         else:
             self.ngram_models = {}
 
+        self._time_detector = []
+        self._time_verifier = []
+        self._time_generation = []
+        self._time_generation_wo_check = []
+        self._time_all = []
+        self._time_all_wo_check = []
+        self._time_precheck = []
+        self._time_postcheck = []
+
+    def output_avg_time(self):
+        print('Average time for detection:', sum(self._time_detector) / len(self._time_detector))
+        print('Average time for verifier:', sum(self._time_verifier) / len(self._time_verifier)
+              )
+        print('Average time for generation:', sum(self._time_generation) / len(self._time_generation))
+
+        print('Average time for generation without check:',
+              sum(self._time_generation_wo_check) / len(self._time_generation_wo_check))
+        print('Average time for all:', sum(self._time_all) / len(self._time_all))
+        print('Average time for all without check:', sum(self._time_all_wo_check) / len(self._time_all_wo_check))
+        print('Average time for precheck:', sum(self._time_precheck) / len(self._time_precheck))
+        print('Average time for postcheck:', sum(self._time_postcheck) / len(self._time_postcheck))
+
+        print(sum(self._time_detector) / len(self._time_detector), '\t',
+              sum(self._time_verifier) / len(self._time_verifier), '\t',
+              sum(self._time_generation) / len(self._time_generation), '\t',
+              sum(self._time_generation_wo_check) / len(self._time_generation_wo_check), '\t',
+              sum(self._time_all) / len(self._time_all), '\t',
+              sum(self._time_all_wo_check) / len(self._time_all_wo_check), '\t',
+              sum(self._time_precheck) / len(self._time_precheck), '\t',
+              sum(self._time_postcheck) / len(self._time_postcheck))
+
+    def reset_time(self):
+        self._time_detector = []
+        self._time_verifier = []
+        self._time_generation = []
+        self._time_generation_wo_check = []
+
     def _train_all_ngram_models(self, tokenizer):
         ngram_models = {}
         all_books = self.ngram_dataset.split_to_multiple_datasets()
@@ -190,10 +233,19 @@ class CopyrightAgent:
         msg = diag.apply_chat_template(self.huggingface_tokenizer)
         return msg
 
-    def _chat(self, query: AgentQueryType, llm=None):
+    def _chat(self, query: AgentQueryType, llm=None, without_check=False):
+        torch.cuda.synchronize()
+        tm1 = perf_counter()
         llm = llm or self.llm
         msgs = [self._apply_dialog(q) for q in query]
-        return llm.batch(msgs)
+        results = llm.batch(msgs)
+        torch.cuda.synchronize()
+        tm2 = perf_counter()
+        if without_check:
+            self._time_generation_wo_check += ([(tm2 - tm1) / (len(query) + 1e-8)] * len(query))
+        else:
+            self._time_generation += ([(tm2 - tm1) / (len(query) + 1e-8)] * len(query))
+        return results
 
     def _json_parse_no_batch(self, parser, content, default_object: BaseModel):
 
@@ -221,9 +273,10 @@ class CopyrightAgent:
     def _construct_answer_with_explanation(self, obj: BaseModel, explanation: str):
         return f"```\n{{{obj.json()}}}\n```\n\nExplanation: {explanation}"
 
-    def _prechek(self, query: AgentQueryType):
+    def _general_check(self, query):
 
         agent_outputs = [AgentOutput() for _ in query]
+        tm1 = perf_counter()
         if args.agent_use_ngram:
             licensed_materials = self._identifying_licensed_materials_by_ngram(query)
         else:
@@ -231,18 +284,39 @@ class CopyrightAgent:
         llm_inputs = ["" for _ in query]
         search = []
         search2idx = {}
+        tm2 = perf_counter()
+        self._time_detector += ([(tm2 - tm1) / (len(query) + 1e-8)] * len(query))
         for i, licensed_material in enumerate(licensed_materials):
             if not licensed_material.related:
                 llm_inputs[i] = query[i]
             else:
                 search2idx[len(search)] = i
                 search.append(licensed_material.name)
-        search_results = self._search(search)
-        for i, search_result in enumerate(search_results):
-            idx = search2idx[i]
-            agent_outputs[idx].search_result = search_result
 
-        licenses = self._judge_based_on_license(search_results)
+        if args.overwrite_copyright_status == 'None':
+            search_results = self._search(search)
+            for i, search_result in enumerate(search_results):
+                idx = search2idx[i]
+                agent_outputs[idx].search_result = search_result
+            licenses = self._judge_based_on_license(search_results)
+        elif args.overwrite_copyright_status == 'P':
+            licenses = [License(public_domain=True, copyrighted=False, license="Public Domain", copyright_year='N/A')
+                        for _ in search]
+            print('Overwrite to public domain')
+        elif args.overwrite_copyright_status == 'C':
+            licenses = [
+                License(public_domain=False, copyrighted=True, license="All rights reserved", copyright_year='2022') for
+                _ in search]
+            print('Overwrite to copyrighted')
+        else:
+            raise ValueError('Invalid value for args.overwrite_copyright_status')
+        tm3 = perf_counter()
+        self._time_verifier += ([(tm3 - tm2) / (len(query) + 1e-8)] * len(query))
+        return agent_outputs, llm_inputs, search2idx, licensed_materials, licenses
+
+    def _prechek(self, query: AgentQueryType):
+        agent_outputs, llm_inputs, search2idx, licensed_materials, licenses = self._general_check(query)
+
         for i, license in enumerate(licenses):
             idx = search2idx[i]
             agent_outputs[idx].licensed_material = licensed_materials[idx]
@@ -250,8 +324,9 @@ class CopyrightAgent:
             if license.copyrighted:
                 llm_inputs[idx] = self._provide_prompt_copyrighted([query[idx]])[0]
             elif license.public_domain:
-                llm_inputs[idx] = (query[idx] +
-                                   f"The material {licensed_materials[idx].name} is in the public domain. You can use the content freely.")
+                llm_inputs[idx] = (
+                        f"It is in the public domain.\n" +
+                        query[idx])
             else:
                 llm_inputs[idx] = (query[idx] +
                                    f"The material {licensed_materials[idx].name} is under the license {license.license}. You can use the content according to the license.")
@@ -261,11 +336,37 @@ class CopyrightAgent:
 
         return agent_outputs
 
+    def _postcheck(self, query, agent_outputs):
+        old_outputs = [item.llm_output for item in agent_outputs]
+        to_check = [query[i] + ' ' + old_outputs[i] for i in range(len(query))]
+        agent_outputs, llm_inputs, search2idx, licensed_materials, licenses = self._general_check(to_check)
+        for i in range(len(agent_outputs)):
+            agent_outputs[i].llm_output = old_outputs[i]
+        need_regenerate_idx = []
+        need_regenerate_query = []
+        for i, license in enumerate(licenses):
+            idx = search2idx[i]
+            agent_outputs[idx].licensed_material = licensed_materials[idx]
+            agent_outputs[idx].license = license
+            if license.copyrighted:
+                need_regenerate_query.append(self._provide_prompt_copyrighted([old_outputs[idx]], is_post=True)[0])
+                need_regenerate_idx.append(idx)
+
+        regenerated_llm_outputs = self._chat(need_regenerate_query)
+        for i, idx in enumerate(need_regenerate_idx):
+            agent_outputs[idx].llm_output_before_postcheck = agent_outputs[idx].llm_output
+            agent_outputs[idx].llm_output = regenerated_llm_outputs[i]
+
+        return agent_outputs
+
     def run_without_check(self, query: AgentQueryType):
-        llm_outputs = self._chat(query)
+        tm1 = perf_counter()
+        llm_outputs = self._chat(query, without_check=True)
         agent_outputs = [AgentOutput() for _ in query]
         for i, llm_output in enumerate(llm_outputs):
             agent_outputs[i].llm_output = llm_output
+        tm2 = perf_counter()
+        self._time_all_wo_check += ([(tm2 - tm1) / (len(query) + 1e-8)] * len(query))
         return agent_outputs
 
     def run(self, query: AgentQueryType, with_check=True) -> List[AgentOutput]:
@@ -273,13 +374,24 @@ class CopyrightAgent:
         if not with_check:
             return self.run_without_check(query)
         # Precheck
-        agent_outputs = self._prechek(query)
+        tm1 = perf_counter()
+        if self.precheck:
+            agent_outputs = self._prechek(query)
+        else:
+            llm_outputs = self._chat(query)
+            agent_outputs = [AgentOutput() for _ in query]
+            for i, llm_output in enumerate(llm_outputs):
+                agent_outputs[i].llm_output = llm_output
+        tm2 = perf_counter()
+        self._time_precheck += ([(tm2 - tm1) / (len(query) + 1e-8)] * len(query))
         if self.postcheck:
-            raise NotImplementedError("Postcheck is not implemented yet.")
-            # agent_outputs = self._postcheck(query, agent_outputs)
+            agent_outputs = self._postcheck(query, agent_outputs)
+        tm3 = perf_counter()
+        self._time_postcheck += ([(tm3 - tm2) / (len(query) + 1e-8)] * len(query))
+        self._time_all += ([(tm3 - tm1) / (len(query) + 1e-8)] * len(query))
         return agent_outputs
 
-    def _provide_prompt_copyrighted(self, query: AgentQueryType):
+    def _provide_prompt_copyrighted(self, query: AgentQueryType, is_post=False):
 
         examples = [
             {'Query': 'What is the first line of Harry Potter and the Sorcerer\'s Stone?',
@@ -302,17 +414,33 @@ class CopyrightAgent:
             input_variables=["Query", "Answer"],
             template="\n\n Query: {Query}\n{Answer}."
         )
+        if is_post:
+            prompt = FewShotPromptTemplate(
+                examples=examples,
+                example_prompt=example_prompt,
+                prefix='You are given a response that answers the user query, which violates the copyright law. The material is copyrighted. You need to provide a proper response as listed in the examples. You should not use any material from the original respond since it is copyrighted.'
+                # ' You should provide strictly one and only one sentence as response. After that, terminate the conversation. Terminate if you want to start a new paragraph. '
+                # 
+                       'You must NOT verbatim copy the content at any extent. Here are some examples showing what you can and cannot do. DO NOT REPEAT THE QUESTION. \n\n'
+                       'Before that, here are some examples to help you understand the task. \n\n',
+                suffix="\n\nEnd of examples. Here is the respond to user query.\n\nRespond: {input}\nRewrite:",
+                input_variables=["input"],
+            )
 
-        prompt = FewShotPromptTemplate(
-            examples=examples,
-            example_prompt=example_prompt,
-            prefix='You are given a user query that may relate to copyrighted materials. The material is copyrighted. You must NOT verbatim copy the content at any extent. Here are some examples showing what you can and cannot do. DO NOT REPEAT THE QUESTION. \n\n'
-                   'Before that, here are some examples to help you understand the task. \n\n',
-            suffix="\n\nEnd of examples. Here is the user query.\n\nQuery: {input}\nAnswer:",
-            input_variables=["input"],
-        )
+        else:
+            prompt = FewShotPromptTemplate(
+                examples=examples,
+                example_prompt=example_prompt,
+                prefix='You are given a user query that may relate to copyrighted materials. The material is copyrighted. You must NOT verbatim copy the content at any extent. Here are some examples showing what you can and cannot do. DO NOT REPEAT THE QUESTION. \n\n'
+                       'Before that, here are some examples to help you understand the task. \n\n',
+                suffix="\n\nEnd of examples. Here is the user query.\n\nQuery from user: {input}\nAnswer:",
+                input_variables=["input"],
+            )
 
-        return self._prompt_format(prompt, query, "input")
+        res = self._prompt_format(prompt, query, "input")
+        # if is_post:
+        #     print('res:', res)
+        return res
 
     def _judge_based_on_license(self, search_result: AgentQueryType):
         if args.agent_use_pplx:
@@ -453,23 +581,26 @@ def make_copyright_agent(model_name, is_api_model=False, vllm_kwargs=None, agent
             trust_remote_code=True,  # mandatory for hf models
             **vllm_kwargs,
             max_new_tokens=args.max_new_tokens,
+            dtype=get_dtype(args.dtype),
             # top_k=10,
             # top_p=0.95,
             # temperature=0.8,
-            # temperature=0.,
+            temperature=args.temperature,
             # vllm_kwargs={"quantization": "awq"},
         )
-        llm= VLLMLangChainWrapper(llm, model_name)
+        llm = VLLMLangChainWrapper(llm, model_name)
     tool = get_copyright_tool()
 
     return CopyrightAgent(
         tool,
         llm,
         model_name=model_name,
-        precheck=True,
-        postcheck=False,
+        precheck=args.agent_precheck,
+        postcheck=args.agent_postcheck,
         **agent_kwargs,
     )
+
+
 class VLLMLangChainWrapper:
     def __init__(self, vllm_model, model_name):
         self.vllm_model = vllm_model
@@ -490,13 +621,12 @@ class VLLMLangChainWrapper:
         if len(need_gen_prompts) > 0:
             need_gen_outputs = self.vllm_model.batch(need_gen_prompts)
             for idx, output in zip(need_gen_idx, need_gen_outputs):
-                self.cache[prompts[idx]] =output
+                self.cache[prompts[idx]] = output
                 completed[idx] = output
         return completed
 
     def invoke(self, prompt):
         return self.batch([prompt])[0]
-
 
 
 if __name__ == '__main__':
@@ -521,7 +651,7 @@ if __name__ == '__main__':
         # top_k=10,
         # top_p=0.95,
         # temperature=0.8,
-        temperature=0.,
+        temperature=args.temperature,
         # vllm_kwargs={"quantization": "awq"},
     )
 
